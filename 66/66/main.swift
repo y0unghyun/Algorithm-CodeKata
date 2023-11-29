4f9c7ce8f2616570220b9f7e900dbff0ccbbd98a
y0unghyun <ikim4127@gmail.com>
2023-11-29 20:56:29 +0900

    #66 [프로그래머스] 대충 만든 자판 업로드

--- 66/66/main.swift
+++ 66/66/main.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
+    
+    var keyDict: [Character: Int] = [:]
+    var answer: [Int] = [], result = 0
+    var targets = targets
+    
+    for key in keymap {
+        var i = 1
+        for k in key {
+            if keyDict[k] == nil {
+                keyDict[k] = i
+            } else {
+                keyDict[k] = keyDict[k]! <= i ? keyDict[k] : i
+            }
+            i += 1
+        }
+    }
+    
+    for (index, target) in targets.enumerated() {
+        for char in target {
+            if let count = keyDict[char] {
+                result += count
+            } else { 
+                result = -1
+                break
+            }
+        }
+        answer.append(result)
+        result = 0
+    }
+    
+    return answer
+}
+
+print(solution(["AA"], ["B"]))
---
+++ 67/67/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  67
+//
+//  Created by 영현 on 11/29/23.
+//
+
+import Foundation
+
+func solution(_ s:String, _ skip:String, _ index:Int) -> String {
+    return ""
+}
