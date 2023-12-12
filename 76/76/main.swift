cd4cbec6ae959b587d6ca38a6b69cd321e752f54
y0unghyun <dl3rpika@gmail.com>
2023-12-12 11:57:17 +0900

    #76 [프로그래머스] JadenCase 문자열 만들기 업로드

---
+++ 76/76/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  76
+//
+//  Created by 영현 on 12/12/23.
+//
+
+func solution(_ s:String) -> String {
+    var input = Array(s), count = 0, temp = [String]()
+    
+    for thing in input {
+        if Int(String(thing)) == nil {
+            if count == 0 && thing != " " {
+                temp.append(thing.uppercased())
+                count += 1
+            } else if thing == " " {
+                temp.append(" ")
+                count = 0
+            } else {
+                temp.append(thing.lowercased())
+            }
+        } else if let intThing = Int(String(thing)) {
+            temp.append(String(intThing))
+            count += 1
+        }
+    }
+    return temp.joined()
+}
+
+print(solution(" a 123e "))
