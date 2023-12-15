826708f1f2e3a01000be471e98e41272de5326c9
y0unghyun <dl3rpika@gmail.com>
2023-12-15 13:26:26 +0900

    #79 [프로그래머스] 카펫 업로드

--- 79/79/main.swift
+++ 79/79/main.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    return []
+    let total = brown + yellow
+    var x = 0, y = 0, result = [Int]()
+    for i in 1...total {
+        if total % i == 0 {
+            x = i
+            y = total / i
+            
+            if (x - 2) * (y - 2) == yellow {
+                break
+            }
+        }
+    }
+    result = [x, y]
+    return result.sorted(by: >)
 }
+
+print(solution(4004,999999))
